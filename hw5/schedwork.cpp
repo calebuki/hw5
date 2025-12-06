b6383bf3bf6a95a511b089334931319c58b30c62
Caleb Swan <ciswan@usc.edu>
2025-12-06 04:46:45 +0000

    final commit?

--- hw5/schedwork.cpp
+++ hw5/schedwork.cpp
@@ -21,7 +21,13 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 
 // Add prototypes for any helper functions here
-
+bool backtracker(const AvailabilityMatrix& avail,
+                 const size_t dailyNeed,
+                 const size_t maxShifts,
+                 DailySchedule& sched,
+                 int day,
+                 int col,
+                 std::vector<int>& worker_shifts);
 
 // Add your implementation of schedule() and other helper functions here
 
@@ -38,8 +44,76 @@ bool schedule(
     sched.clear();
     // Add your code below
 
+    int days = avail.size();
+    int workers = avail[0].size();
 
+    sched.resize(days);
+    for (int i = 0; i < days; i++){
+      sched[i].resize(dailyNeed, INVALID_ID);
+    }
+
+    //keep track of how many shifts per worker
+    std::vector<int> worker_shifts(workers, 0);
+
+    return backtracker(avail, dailyNeed, maxShifts, sched, 0, 0, worker_shifts);
+
+//sched works like
+//sched[`day`][`slot`] and outputs the worker #
 
 
 }
 
+bool backtracker(const AvailabilityMatrix& avail,
+                 const size_t dailyNeed,
+                 const size_t maxShifts,
+                 DailySchedule& sched,
+                 int day,
+                 int col,
+                 std::vector<int>& worker_shifts){
+  //start
+
+  //if we are at the end of the col, move to next day
+  if (col == dailyNeed) {
+    return backtracker(avail, dailyNeed, maxShifts, sched, day + 1, 0, worker_shifts);
+  }
+
+  //base case: all days are accounted for
+  if (day == avail.size()){
+    return true;
+  }
+
+  for (Worker_T worker = 0; worker < avail[0].size(); ++worker){
+    //check if the worker is available
+    if (avail[day][worker] == true){
+
+      //check shift limit
+      if (worker_shifts[worker] < maxShifts){
+
+        //just because they are available != they are not working!
+        //if they aren't already working today, TRY:
+        bool working_today = false;
+        for (int i = 0; i < col; i++){
+          if (sched[day][i] == worker){
+            working_today = true;
+          }
+        }
+
+        if (!working_today){
+          sched[day][col] = worker;
+          worker_shifts[worker]++;
+
+          //recurse until all rows and cols are hit
+          if (backtracker(avail, dailyNeed, maxShifts, sched, day, col + 1, worker_shifts)){
+            return true;
+          }
+
+          //BACKTRACK HERE, we hit the "false"
+          worker_shifts[worker]--;
+          sched[day][col] = INVALID_ID;
+        }
+      }
+    }
+  }
+  //if no worker fits, then this path was wrong and backtrack
+  return false;
+}
--- hw5/wordle.cpp
+++ hw5/wordle.cpp
@@ -13,6 +13,13 @@ using namespace std;
 
 
 // Add prototypes of helper functions here
+void wordFinder(const std::string &in, 
+                std::string yellow, 
+                std::string current_word,
+                const std::set<std::string> &dict,
+                std::set<std::string> &words,
+                int index,
+                int blanks);
 
 
 // Definition of primary wordle function
@@ -22,7 +29,72 @@ std::set<std::string> wordle(
     const std::set<std::string>& dict)
 {
     // Add your code here
+    int blanks = 0;
+    std::set<std::string> words;
+
+    for (int i = 0; i < in.length(); i++){
+        if (in[i] == '-'){
+            blanks++;
+        }
+    }
+
+    wordFinder(in, floating, in, dict, words, 0, blanks);
+    return words;
 
 }
 
 // Define any helper functions here
+void wordFinder(const std::string &in, 
+                std::string yellow, 
+                std::string current_word,
+                const std::set<std::string> &dict,
+                std::set<std::string> &words,
+                int index,
+                int blanks){
+    
+    //base case: if the word `in` is filled, then check if its real
+    if (index == in.length()){
+        if (yellow.empty() && dict.find(current_word) != dict.end()){
+            words.insert(current_word);
+        }
+        return;
+    }
+    
+    //recursive case: only change around '-' characters
+    if (in[index] == '-'){
+        //try every letter?
+        for (char i = 'a'; i <= 'z'; i++){
+
+            //check if `i` is a yellow char
+            //if not found, yellow_index > length of
+            int yellow_index = yellow.find(i);
+
+            bool is_yellow = yellow_index < yellow.length();
+
+            if (is_yellow == true){
+              std::string next_yellow = yellow;
+              next_yellow.erase(yellow_index, 1);
+
+              //put the current char in the current_word
+              current_word[index] = i;
+
+              //go agane!!
+              wordFinder(in, next_yellow, current_word, dict, words, index + 1, blanks - 1);
+
+            } else {
+              //it's not a yellow letter
+              if (blanks > yellow.length()){
+                current_word[index] = i;
+
+                //move on
+                wordFinder(in, yellow, current_word, dict, words, index + 1, blanks - 1);
+              }
+            }
+        }
+
+    } else {
+        //recurse cuz current index is not a blank
+        wordFinder(in, yellow, current_word, dict, words, index + 1, blanks);        
+    }
+
+}
